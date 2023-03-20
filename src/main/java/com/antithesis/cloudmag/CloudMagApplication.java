1553bcd0ca53bf09cd71a967a9f4b0bb373b3906
Andres F Morales A <anfelpe.0200@gmail.com>
2023-03-20 16:22:57 -0500

    Add initial configuration

---
+++ src/main/java/com/antithesis/cloudmag/CloudMagApplication.java
@@ -0,0 +1,13 @@
+package com.antithesis.cloudmag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudMagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudMagApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/antithesis/cloudmag/CloudMagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antithesis.cloudmag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudMagApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
